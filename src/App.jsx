e626193ec847947710088254ad5c10692d9c423f
Daniel Owen <unlimiteddemi@gmail.com>
2025-09-13 05:11:27 +0100

    fixed base url

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ L.Icon.Default.mergeOptions({
 });
 
 // --- Configuration ---
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = "https://sat-backend-55lg.onrender.com"
 
 // --- SVG Icons ---
 const PipelineIcon = () => <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" /></svg>;
